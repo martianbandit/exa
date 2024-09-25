0111291e521b8f77ec1f3ee78cb6bde5eaafed2c
Jo Paradis <154098629+martianbandit@users.noreply.github.com>
2024-09-25 20:42:27 +0000

    first

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import os
+from exa_py import Exa
 
+exa = os.environ(api_key="EXA_API_KEY")
+
+result = exa.search_and_contents(
+  "le chant des baleines",
+  type="auto",
+  num_results=10,
+  text=True,
+  livecrawl="always",
+  summary=True
+)
